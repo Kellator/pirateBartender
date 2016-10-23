02a5353f93190643f8644dff01979d3f02cd1c86
Kellator <k.macleod21@gmail.com>
2016-10-23 18:41:05 -0400

    render functions added, event listeners added

--- app.js
+++ app.js
@@ -2,40 +2,66 @@
 //state of the app
 var state = {
 	questions:[
-	{	
-		text: "Do ye like yer drinks on the rocks or frozen?",
-		choices: ["On the rocks, just like me ol' ship", "I'll take me grog frozen like me heart."],
-	}
 	{
 		text: "Which friend are ye drinking with tonight?",
 		choices: ["I'm sailin' with the Captain", "Ah! Mi Amigo es Jose'", "Me buddy Jack and I are passing thorugh on our way to Tennessee", "Goose is my first-mate tonight", "Have ye met me ol' lady, Shirley?"],
-	}
+	},
+	{	
+		text: "Do ye like yer drinks on the rocks or frozen?",
+		choices: ["On the rocks, just like me ol' ship", "I'll take me grog frozen like me heart."],
+	},
 	{
 		text: "Will ye be having a wee bit o' sweetness in your grog this evening?",
 		choices: ["Aye!", "I think I'm sweet enough"],
-	}
+	},
 	{
 		text: "Would you like your grog with a salty tang?",
 		choices: ["Aye, I like me drinks like a like me sea!", "No! I've had quite enough bilgewater, thank you."],
-	}
+	},
 	{
 		text: "Would you be a Son of a Biscuit Eater who be likin' 'is drinks a bit bitter?",
 		choices: ["Aye, you know me well", "Bitter, aye perhaps I am, but not for me drink"],
-	}
+	},
 	{
 		text: "Are ye a scallywag who likes a little extra booty to decorate yer drink?",
 		choices: ["Aye. load 'er up!", "No! Don't you be scuttlin' me drink with your garrrrnishes!"],
-	}
+	},
+	],
+//lists of mixer ingredients for each question 
+	liquorIngredients: [
+		"Swig of Rum", 
+		"Shot of Tequila", 
+		"Dram of Whiskey", 
+		"Pour of Vodka", 
+		"Spritz of Soda"
+	],
+	sweetIngredients: [
+		"Drop of pineapple juice", 
+		"Squeeze of orange juice", 
+		"Drizzle of raspberry puree swirl", 
+		"Bit of coconut liqueur", 
+		"Splash of sweet and sour mix"
+	],
+	saltyIngredients: [
+		"Olive on a tiny sword", 
+		"Salt-rimmed glass", 
+		"Pickle brine"
+	],
+	bitterIngredients: [
+		"Shake of bitters", 
+		"Squirt of tonic", 
+		"Twist of Lemon"
+	],
+	garnishIngredients: [
+		"Wedge of lime", 
+		"Slice of orange", 
+		"A cherry on top", 
+		"Swizzle straw"
 	],
 
-	liquorIngredients: ["Swig of Rum", "Shot of Tequila", "Dram of Whiskey", "Pour of Vodka", "Spritz of Soda"],
-	sweetIngredients: ["Drop of pineapple juice", "Squeeze of orange juice", "Drizzle of raspberry puree swirl", "Bit of coconut liqueur", "Splash of sweet and sour mix"],
-	saltyIngredients: ["Olive on a tiny sword", "Salt-rimmed glass", "Pickle brine"],
-	bitterIngredients: ["Shake of bitters", "Squirt of tonic", "Twist of Lemon"],
-	garnishIngredients: ["Wedge of lime", "Slice of orange", "A cherry on top", "Swizzle straw"],
-
 	route: "start",
-	currentQuestionIndex: 0,
+	currentQuestionIndex: 0;
+	mixerRandom: 0;
 }
 
 //constructor functions
@@ -44,12 +70,89 @@ var state = {
 function questions(text, choices) {
 	this.text = text;
 	this.choices = choices;
-}
+};
 //ingredients constuctor function
 function ingredients(mixers) {
 	this.mixers = mixers;
-}
+	console.log(mixers);
+};
 //pantry constructor function
 function pantry(supplies) {
 	this.supplies = supplies;
+};
+
+//functions that affect state
+//start drink mixer and determine where is program user is (i.e. at start_page, questions_page, or drink_page)
+function setRoute (state, route) {
+	state.route = route;
+};
+//reset the questions and mix a new drink
+function resetBartender (state) {
+	state.currentQuestionIndex = 0;
+	setRoute(state, "start");
+};
+//selects random ingredients from mixers lists
+function selectMixer(state) {
+	state.mixerRandom= Math.random();
+};
+
+//asks the questions and stores the answers from user
+//moves through the questions and sets page_element
+function nextQuestion(state) {
+	state.currentQuestionIndex++;
+	if (state.currentQuestionIndex === state.questions.length) {
+		setRoute(state, "drink");
+	}
+	else {
+		setRoute(state, "question");
+	}
 }
+
+//render functions
+
+//function renderStartPage() {};
+
+//function renderQuestionsPage() {};
+
+//function renderDrinkPage() {};
+
+//function renderQuestionText() {};  
+
+	//write the html for the input
+//function renderChoices() {};  
+
+	//changes text to be displayed in the next question button based on whether bartender has additional questions or drink is ready to be mixed
+//function renderNextButtonText(state, element) {
+// 	var text = state.currentQuestionIndex < state.questions.length -1 ? "Anymore questions, bartender?" : "Ok, mix me drink already!";
+// 	element.text(text);
+// };
+
+
+
+//event listeners
+
+var page_elements = {
+	"start": $(".start_page"),
+	"question": $(".questions_page"),
+	"drink": $(".drink_page")
+};
+//bartender start button listener
+$("form[name='start_mixing']").submit(function(event) {
+	event.preventDefault();
+	setRoute(state, "question");
+	renderApp(state, page_elements);
+});
+//reset bartender listener
+$(".reset_questions").click(function(event){
+	event.preventDefault();
+	resetBartender(state);
+	renderApp(state, page_elements);
+});
+//answer submit listener
+$(".submit_choice").click(function(event) {
+	event.preventDefault();
+	nextQuestion(state);
+	renderApp(state, page_elements);
+});
+
+$(function() {renderApp(state, page_elements);});
